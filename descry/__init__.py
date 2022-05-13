ce061fed94cbb95e40981a1795b2d6b3df5596aa
David Freifeld <freifeld.david@gmail.com>
2022-05-12 23:54:21 -0700

    Initi
    q

--- descry/__init__.py
+++ descry/__init__.py
@@ -1,1 +1,15 @@
 __version__ = '0.1.0'
+
+import torch
+from transformers import ViTConfig, ViTFeatureExtractor, ViTModel
+
+
+class VisionTransformer(torch.nn.Module):
+    def __init__(self):
+        super(VisionTransformer, self).__init__()
+        self.feature_extractor = ViTFeatureExtractor.from_pretrained("google/vit-base-patch16-224-in21k")
+        self.model = ViTModel.from_pretrained("google/vit-base-patch16-224-in21k")
+
+    def forward(self, image):
+        inputs = self.feature_extractor(image, return_tensors="pt")
+        return self.model(**inputs)
--- tests/test_descry.py
+++ tests/test_descry.py
@@ -1,5 +1,10 @@
-from descry import __version__
+import torch
+from descry import VisionTransformer, __version__
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_transformer():
+    vt = VisionTransformer()
+    vt.forward(torch.random(10, 3, 256, 256))
