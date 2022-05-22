ca999a5a15678cecb5d5d8a3fef5626e44e0cd8f
David Freifeld <freifeld.david@gmail.com>
2022-05-22 11:07:48 -0700

    Experiment with architecture + add image padding
    Switching to a more ConvTranspose2d based system. Padding all images
    to 1024x1024 now for convienience's sake.

--- descry/__init__.py
+++ descry/__init__.py
@@ -1,15 +1,52 @@
 __version__ = '0.1.0'
 
+import os
+
 import torch
+from PIL import Image
+from torch import nn
+from torch.utils.data import Dataset
 from transformers import ViTConfig, ViTFeatureExtractor, ViTModel
 
 
-class VisionTransformer(torch.nn.Module):
+class FashionDataset(Dataset):
+    def __init__(self, path):
+        self.path = path
+
+    def __len__(self):
+        return 1000 # TODO generalize
+
+    def __getitem__(self, n):
+        raw_image = Image.open(os.path.join(self.path, f"png_images/IMAGES/img_{n:04}.png"))
+        raw_mask = Image.open(os.path.join(self.path, f"png_masks/MASKS/seg_{n:04}.png"))
+        return (raw_image, raw_mask)
+
+class VisionTransformer(nn.Module):
     def __init__(self):
         super(VisionTransformer, self).__init__()
         self.feature_extractor = ViTFeatureExtractor.from_pretrained("google/vit-base-patch16-224-in21k")
         self.model = ViTModel.from_pretrained("google/vit-base-patch16-224-in21k")
+        self.head = nn.Sequential(
+            nn.Conv2d(1, 8, (8, 8), stride=2),
+            nn.MaxPool2d(4),
+            nn.Conv2d(8, 1, (4, 4)),
+            nn.ReLU(),
+            # nn.Flatten(1),
+            # nn.Linear(1840, 512),
+            # nn.Linear(512, 128),
+            # nn.Linear(128, 512),
+            # nn.Linear(512, 1000),
+            nn.ConvTranspose2d(1, 8, 16),
+            nn.ConvTranspose2d(8, 1, 32),
+            nn.ConvTranspose2d(1, 1, 128),
+            # nn.Conv2d(8, 1, (4, 4)),
+        )
 
-    def forward(self, image):
+    def forward(self, image, mask):
         inputs = self.feature_extractor(image, return_tensors="pt")
-        return self.model(**inputs)
+        out = self.model(**inputs).last_hidden_state
+        # out = torch.reshape(out, (197, 768))
+        print(self.head(out).shape)
+
+    # 550x825
+# 453750
--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+import matplotlib.pyplot as plt
 import torch
-from datasets import load_dataset
+from PIL import Image
+from torch import nn
+from torchvision.transforms import ToPILImage, ToTensor
 
-from descry import VisionTransformer
+from descry import FashionDataset, VisionTransformer
 
 vt = VisionTransformer()
-dataset = load_dataset("huggingface/cats-image")
-image = dataset["test"]["image"][0]
-vt.forward(image)
+# dataset = load_dataset("huggingface/cats-image")
+# image = dataset["test"]["image"][0]
+# print(image.shape)
+data = FashionDataset("/home/quantumish/aux/fashion-seg/")
+image = data[3][0]
+image = ToTensor()(image)
+print(image.shape)
+# print(1024-image.shape[1], 1024-image.shape[2])
+image = nn.functional.pad(image, (237, 237, 100, 99), mode="replicate")
+
+print(type(image), image.size)
+vt.forward(image, image)
